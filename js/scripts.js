0b8389d0cd579523bd5373e29caa07ddc9698942
LukeCallahan <lukemcal@gmail.com>
2022-10-28 13:46:39 -0700

    pass test to get the words converting for any value of 1,2, or 3.

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,26 @@ function wordReplacer(number){
   for (let i=1; i < inputNumber + 2; i += 1){
     arrayElement += 1;
     stringArray[i] = arrayElement -1;
-    console.log(stringArray[i]);
   };
-  
-  stringArray = number.split("");
+
+  //Okay so here, stringArray has our entire array of numbers from 0 to 14. It says it is undefined.
+  // So from here we want to take this array, iterate through it, then, on each Element, we want to split it into it's own array, determine it's length, and if it has a 1, 2, or 3, in it, perform a function.
+
+
+
+
+
+
+  // stringArray = number.split("");
   for (let i = 0; i < stringArray.length; i += 1){
-    if (stringArray[i] === 3){
+    if (stringArray[i].toString().includes("3")){
       stringArray[i] = "Won't you be my neighbor?";
-    } else if (stringArray[i] === 2){
+    } else if (stringArray[i].toString().includes("2")){
       stringArray[i] = "Boop";
-    } else if (stringArray[i] === 1){
+    } else if (stringArray[i].toString().includes("1")){
       stringArray[i] = "Beep";
     }
+    console.log(stringArray[i]);
   };
   return stringArray;
 }
@@ -40,7 +48,6 @@ window.addEventListener("load", function(){
   form.addEventListener("submit", function(event){
     event.preventDefault();
     const number = document.getElementById("text-input1").value;
-    console.log(number);
     let test = wordReplacer(number);
     let div = document.createElement("div");
     let p = document.createElement("p");
